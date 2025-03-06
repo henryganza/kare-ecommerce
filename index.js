2eb6b8ba14c03dad26415af1de5f6974d667a5f0
Your Name <you@example.com>
2025-03-05 17:11:26 -0800

    Added README.md, index.js, and products.js

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Karé!</h1>
+      <p>Your #1 E-Commerce Store in Kigali</p>
+    </div>
+  );
+}
---
+++ products.js
@@ -0,0 +1,8 @@
+export default function Products() {
+  return (
+    <div>
+      <h1>Our Products</h1>
+      <p>Check out our amazing products!</p>
+    </div>
+  );
+}
